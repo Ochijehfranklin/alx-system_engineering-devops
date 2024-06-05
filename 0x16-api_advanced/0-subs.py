ed2331b1957e4675ab6ce0c5a447302e220696bf
ochijeh Franklin <ochijehfranklin@gmail.com>
2024-06-05 18:44:23 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,6 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'xica369'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 200:
         return (response.json().get("data").get("subscribers"))
     return (0)
